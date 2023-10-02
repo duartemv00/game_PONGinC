02a03083d8d0835312cc7de6b9dd220c1a1fc3d5
DuarteMV <duartemv00@gmail.com>
2023-10-02 04:56:10 +0200

    Basic renderer and clamp utility

--- MiFirstGameProject/win32_platform.cpp
+++ MiFirstGameProject/win32_platform.cpp
@@ -1,13 +1,46 @@
 #include <windows.h>
+#include "utilities.cpp"
 
-bool running = true;
+global_variable bool running = true; //check if the game is still running
 
-LRESULT CALLBACK window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-	LRESULT result = 0;
+struct Render_State {
+	int height, width;
+	void* memory;
+
+	BITMAPINFO bitmap_info;
+};
+
+global_variable Render_State render_state;
+
+#include "renderer.cpp"
+
+LRESULT CALLBACK window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) { //process window messages
+	LRESULT result = 0; //default output
 	switch (uMsg) {
 		case WM_CLOSE:
-		case WM_DESTROY: {
+		case WM_DESTROY: { // message when the window closes
 			running = false;
+		} break;
+
+		case WM_SIZE: { //message when de window changes size
+			RECT rect; //stores the position of the window
+			GetClientRect(hwnd, &rect);
+			render_state.width = rect.right - rect.left;
+			render_state.height = rect.bottom - rect.top;
+
+			int size = render_state.width * render_state.height * sizeof(unsigned int);
+
+			if (render_state.memory) VirtualFree(render_state.memory, 0, MEM_RELEASE);
+			render_state.memory = VirtualAlloc(0, size, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+
+			render_state.bitmap_info.bmiHeader.biSize = sizeof(render_state.bitmap_info.bmiHeader);
+			render_state.bitmap_info.bmiHeader.biWidth = render_state.width;
+			render_state.bitmap_info.bmiHeader.biHeight = render_state.height;
+			render_state.bitmap_info.bmiHeader.biPlanes = 1;
+			render_state.bitmap_info.bmiHeader.biBitCount = 32;
+			render_state.bitmap_info.bmiHeader.biCompression = BI_RGB;
+
+
 		} break;
 
 		default: {
@@ -29,17 +62,21 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
 
 	// Create Window
 	HWND window = CreateWindow(window_class.lpszClassName, "My First Game", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720, 0, 0, hInstance, 0);
+	HDC hdc = GetDC(window);
 
 	while (running) {
 		// Input
 		MSG message;
 		while (PeekMessage(&message, window, 0, 0, PM_REMOVE)) {
-			TranslateMessage(&message);
-			DispatchMessage(&message);
+			TranslateMessage(&message); //transforms virtual key messages into character messages.
+			DispatchMessage(&message); //dispatches a message to a window procedure
 		}
 
 		// Simulate
+		clear_screen(0xff5500); //sets all the screen to 1 color
+		draw_rectangle(0, 0, 20, 20, 0x00ff22); //draws an square
 
 		// Render
+		StretchDIBits(hdc, 0, 0, render_state.width, render_state.height, 0, 0, render_state.width, render_state.height, render_state.memory, &render_state.bitmap_info, DIB_RGB_COLORS, SRCCOPY);
 	}
 }
